26ae2c741be1b8013c4366a9925e572b9dd7e54f
Hung Nguyen <nphung@elarvee.xyz>
2017-06-04 22:27:08 +0700

    Pagination: First implementation

--- src/components/Mails.vue
+++ src/components/Mails.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="mails">
     <mail-summary v-for="mail in mails" :mail="mail" :key="mail.id"></mail-summary>
+
+    <div class="row pagination">
+      <div class="col text-center">
+        <b-button-group>
+          <b-button @click="previous" :disabled="offset === 0">Previous</b-button>
+          <b-button @click="next" :disabled="mails.length < limit">Next</b-button>
+        </b-button-group>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,13 +24,43 @@
     data: () => ({
       mails: []
     }),
+    computed: {
+      limit () {
+        return parseInt(this.$route.query.limit) || 10
+      },
+
+      offset () {
+        return parseInt(this.$route.query.offset) || 0
+      }
+    },
     created () {
-      axios.get('/mails')
-        .then(({data}) => { this.mails = data || [] })
-        .catch(err => console.log(err))
+      this.fetch(this.limit, this.offset)
+    },
+    beforeRouteUpdate (to, from, next) {
+      this.fetch(to.query.limit, to.query.offset)
+      next()
+    },
+    methods: {
+      fetch (limit, offset) {
+        axios.get('/mails', { params: { limit: limit, offset: offset } })
+          .then(({data}) => { this.mails = data || [] })
+          .catch(err => console.log(err))
+      },
+
+      next () {
+        this.$router.push(`/?limit=${this.limit}&offset=${this.offset + 10}`)
+      },
+
+      previous () {
+        this.$router.push(`/?limit=${this.limit}&offset=${this.offset - 10}`)
+      }
     }
   }
 </script>
 
-<style>
+<style scoped lang="scss">
+  .pagination {
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
 </style>
